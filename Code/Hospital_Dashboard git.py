fe2817a6ba12e33f4dbc3be7f01af46c2542aece
Bonnie McDaniel <bkovales1@students.kennesaw.edu>
2019-04-21 21:58:22 -0400

    Update documentation and initial test code

---
+++ Code/Hospital_Dashboard git.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 20 17:58:16 2019
+
+@author: Student
+"""
+
+import pandas as pd
+
+psi_data = pd.read_csv('c:/Users/Bonnie/Documents/GitHub/Hospital-Dashboard/Data_Clean/psi_data.csv',index_col=0)
+psi_data.info()
